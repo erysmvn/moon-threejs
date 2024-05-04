a09bbd69829fadc81887c9b940dff316c10098f3
erys <sickekklx4l@gmail.com>
2024-05-04 19:37:11 +0200

    3D models control added

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+import { faceDirection } from 'three/examples/jsm/nodes/Nodes.js';
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 const scene = new THREE.Scene();
 
@@ -15,3 +17,38 @@ render.setSize( window.innerWidth, window.innerHeight );
 camera.position.setZ(30);
 
 render.render( scene, camera );
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+const material = new THREE.MeshBasicMaterial( { color: 0xFF6347 } );
+const torus = new THREE.Mesh( geometry, material )
+
+scene.add(torus)
+
+//without light we won't se our shape
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(5,5,5)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, render.domElement);
+
+
+// let's create a function to automate this action
+function animate(){
+  requestAnimationFrame( animate );
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  controls.update();
+
+  render.render( scene, camera );
+}
+
+animate()
